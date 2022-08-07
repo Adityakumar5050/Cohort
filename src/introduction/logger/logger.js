8c0ba03c3c2c43fe364a4e4d1f9f1a4cb1c24abc
Adityakumar5050 <ak8445ch@gmail.com>
2022-08-07 21:46:50 +0530

    update loggers

---
+++ src/introduction/logger/logger.js
@@ -0,0 +1,6 @@
+let welcome = function () {
+    console.log(
+        "welcome to my application . I am Aditya kumar and a part of FunctionUp plutonium cohort"
+    );
+};
+module.exports.funWelcome = welcome;
