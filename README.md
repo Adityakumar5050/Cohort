ec6f653efab096c58e9031afd715e672996ca108
Adityakumar5050 <ak8445ch@gmail.com>
2022-08-07 15:08:54 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# Cohort
+# Plutonium
+Backend cohort Feb 2022 - May 2022
 
-ho ja bhai ab toh 
+
+# Assignment
+https://docs.google.com/document/d/1HanTHEnUW0d2M7KOCTWrnqzwRWOkLUU0X_x_TwWnyTs/edit?usp=sharing
---
+++ src/routes/route.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const abc = require('../introduction/intro')
+const router = express.Router();
+
+router.get('/test-me', function (req, res) {
+    console.log('My batch is', abc.name)
+    abc.printName()
+    res.send('My second ever api!')
+});
+
+
+router.get('/test-you', function(req, res){
+    res.send('This is the second routes implementation')
+})
+
+router.get('/give-me-students-data',function(req, res){
+
+})
+module.exports = router;
+// adding this comment for no reason
