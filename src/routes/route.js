a422f0392fad1c61acfd809eafc81957cc243218
Adityakumar5050 <ak8445ch@gmail.com>
2022-08-07 22:19:14 +0530

    update route

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const abc = require('../introduction/intro')
+const welcome =require('../logger/logger.js')
+const dateMon =require('..util/helper')
+const stringEdit =require('../validator/formatter')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
     abc.printName()
     res.send('My second ever api!')
+    welcome.funWelcome()
+    dateMon.funDateMonth()
+    dateMon.funBatchinfo()
+    stringEdit.trim()
+    stringEdit.changetoLowerCase()
+    stringEdit.changetoUpperCase()
 });
 
 
