ba5ffe29f1ad7f5cffe498f2d2cf1ab31b1938e9
Adityakumar5050 <ak8445ch@gmail.com>
2022-08-07 23:50:00 +0530

    update index

--- src/index.js
+++ src/index.js
@@ -10,6 +10,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', route);
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen(process.env.PORT || 3001, function() {
+    console.log('Express app running on port ' + (process.env.PORT || 3001))
 });
