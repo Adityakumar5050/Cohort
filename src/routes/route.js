8ed24db1010e912a0028d704b0519c75c69ef301
Adityakumar5050 <ak8445ch@gmail.com>
2022-08-09 22:03:50 +0530

    params query & debbuing & git api

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,12 +7,13 @@ router.get('/test-me', function (req, res) {
     abc.printName()
     //logger.welcome()
 
-    res.send('My second ever api!')
+    res.send('my name is aditya kumar!')
+    
 });
 
-router.get('/students', function (req, res){
+router.get('/', function (req, res){
     let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
+    res.send(studentsstudents)
 })
 
 router.get('/student-details/:name', function(req, res){
@@ -37,33 +38,136 @@ router.get('/student-details/:name', function(req, res){
 
 module.exports = router;
 
-// problem 1
-let movies = ['Rang de basanti' ,'The shining', 'Lord of the rings', 'Batman begins']
-router.get("/movies",function(req,res){
 
-    res.send(movies)
-})
+// router.get("/movies/:indexNumber", function(req, res){
+//     const movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+//     console.log(req.params.indexNumber)
+//     let movieIndex = req.params.indexNumber
+//     //check index value. less than 0 or greater than array (length - 1) are not valid
+//     if(movieIndex<0 || movieIndex>=movies.length) {
+//         //if the index is invalid send an error message
+//         return res.send('The index value is not correct, Please check the it')
+//     }
 
-//problem 2
+//     //if the index was valid send the movie at that index in response
+//     let requiredMovie = movies[movieIndex]
+//     res.send(requiredMovie)
+// })
 
-router.get('/movies/:indexNumber',function(req,res){
-    let requestParams=req.params.indexNumber
-    res.send(movies[requestParams])
+// router.get("/shoes", function(req, res){
+//     let queryParams = req.query
+//     let brand = queryParams.brand
+//     res.send("dummy response")
+// })
 
-})
+// uses query params
+// router.get('/candidates', function(req, res){
+//     console.log('Query paramters for this request are '+JSON.stringify(req.query))
+//     let gender = req.query.gender
+//     let state = req.query.state
+//     let district = req.query.district
+//     console.log('State is '+state)
+//     console.log('Gender is '+gender)
+//     console.log('District is '+district)
+//     let candidates = ['Akash','Suman']
+//     res.send(candidates)
+// })
 
-//problem 3
-router.get('movies/:indexNumber',function(req,res){
-    const movies=['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins'];
-    let requestParams=req.params
-    let index=requestParams.indexNumber;
-        if (index>=0 &&index<=movies.length){
-            res.send(movies[index])
-        }
-        else{
-            res.send("not index")
-        }
-    
-})
+// use path param
+// router.get('/candidates/:canidatesName', function(req, res){
+//     console.log('The request objects is '+ JSON.stringify(req.params))
+//     console.log('Candidates name is '+req.params.canidatesName)
+//     res.send('Done')
+// })
 
-module.exports.Router
+// router.get("/films", function(req, res){
+//     const films = [ {
+//         "id": 1,
+//         "name": "The Shining"
+//        }, {
+//         "id": 2,
+//         "name": "Incendies"
+//        }, {
+//         "id": 3,
+//         "name": "Rang de Basanti"
+//        }, {
+//         "id": 4,
+//         "name": "Finding Nemo"
+//        }]
+//        //send all the films
+//       res.send(films) 
+// })
+
+// router.get("/films/:filmId", function(req, res){
+//     const films = [ {
+//         "id": 1,
+//         "name": "The Shining"
+//        }, {
+//         "id": 2,
+//         "name": "Incendies"
+//        }, {
+//         "id": 3,
+//         "name": "Rang de Basanti"
+//        }, {
+//         "id": 4,
+//         "name": "Finding Nemo"
+//        }]
+
+//        let filmId = req.params.filmId
+
+//        //iterate all the films
+//        //search for a film whose id matches with the id recevied in request
+//        for(let i = 0; i < films.length; i++){
+//            let film = films[i]
+//            if(film.id == filmId) {
+//                //if there is a match return the response from here
+//                return res.send(film)
+//            }
+//        }
+
+//        //if there is no match give an error response
+//        res.send("The film id doesn't match any movie")
+// })
+
+// module.exports = router;
+// // adding this comment for no reason
+// Footer
+
+
+app.get("/sol1", function (req, res) {
+     //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+    let arr= [1,2,3,5,6,7]
+  
+     let total = 0;
+     for (var i in arr) {
+         total += arr[i];
+     }
+  
+     let lastDigit= arr.pop()
+     let consecutiveSum= lastDigit * (lastDigit+1) / 2
+     let missingNumber= consecutiveSum - total
+  
+     res.send(  { data: missingNumber  }  );
+   });
+
+//   // -write an api which gives the missing number in an array of integers starting from anywhere….e.g [33, 34, 35, 37, 38]: 36 is missing
+//  app.get("/sol2", function (req, res) {
+//     //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+//     let arr= [33, 34, 35, 37, 38]
+//     let len= arr.length
+  
+//     let total = 0;
+//     for (var i in arr) {
+//         total += arr[i];
+//     }
+  
+//     let firstDigit= arr[0]
+//     let lastDigit= arr.pop()
+//     let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+//     let missingNumber= consecutiveSum - total
+   
+//     res.send(  { data: missingNumber  }  );
+//   });
+ 
+
+  //module.exports = router;
